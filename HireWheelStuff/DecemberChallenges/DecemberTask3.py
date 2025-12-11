4eafabe12b5e7e881065c369f6ff03b06003c9b7
BoopYurNose <zacharybaca07@gmail.com>
2025-12-11 00:22:37 +0000

    :wq

--- HireWheelStuff/DecemberChallenges/DecemberTask3.py
+++ HireWheelStuff/DecemberChallenges/DecemberTask3.py
@@ -1,6 +1,13 @@
+import math #I'm really confused on this. I'll come back to this later. It somewhat works but not the way I want it to.
+
 def NumberChecker(Num):
     if Num >= 2:
-        pass # math stuff here
+        for Num in range(Num, 0, -1):
+            for i in range(2, int(math.sqrt(Num)) + 1):
+                if Num % i == 0:
+                    print(f"sorry {Num} is not a prime number")
+                else:
+                    print(f"{Num} is a prime number!")
     else:
         print(f"{Num} is not a valid number and it does not have any predecessors of numbers that are prime numbers either")
     
