748995b967905f0b8a2426492c38b95b12d15ee0
BoopYurNose <zacharybaca07@gmail.com>
2025-12-21 22:18:33 +0000

    :wq

--- HireWheelStuff/DecemberChallenges/DecemberTask3.py
+++ HireWheelStuff/DecemberChallenges/DecemberTask3.py
@@ -1,16 +1,27 @@
 import math #I'm really confused on this. I'll come back to this later. It somewhat works but not the way I want it to.
 
 def NumberChecker(Num):
-    if Num >= 2:
-        for Num in range(Num, 0, -1):
-            for i in range(2, int(math.sqrt(Num)) + 1):
-                if Num % i == 0:
-                    print(f"sorry {Num} is not a prime number")
-                else:
-                    print(f"{Num} is a prime number!")
-    else:
-        print(f"{Num} is not a valid number and it does not have any predecessors of numbers that are prime numbers either")
+    if Num < 2:
+        print("Sorry You have to choose a number that is above 2 or is 2 itself")
+        return
     
+
+    for Num in range(Num, 1, -1):
+        Prime = True
+        
+        for i in range(2, int(math.sqrt(Num)) + 1):
+            if Num % i == 0:
+                Prime = False
+                break
+            
+            if Prime:
+                print(f"{Num} this is a prime number")
+            else:
+                print(f"{Num} This is not a prime number")
+        
+
+        
+
 
 UserNumber = input("Type in a number to check if it's a prime number or not: ")
 
