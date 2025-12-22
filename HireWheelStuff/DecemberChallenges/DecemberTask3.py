0090c90f45b3660ef8a0fabfe83831438ad79e55
BoopYurNose <zacharybaca07@gmail.com>
2025-12-21 18:10:24 -0600

    Update DecemberTask3.py

--- HireWheelStuff/DecemberChallenges/DecemberTask3.py
+++ HireWheelStuff/DecemberChallenges/DecemberTask3.py
@@ -1,5 +1,11 @@
 import math #I'm really confused on this. I'll come back to this later. It somewhat works but not the way I want it to.
 
+#12/21/25 Ive worked on this for like 3 hours trying to understand how it works and how to fix it
+# I found the solution online and everything and sure i could just copy and paste the solution here
+# and act like I made it but that would do nothing for me, i actually want to uneerstand how eqch part
+#of it works without just fucking copying and pasting i actually want to comprehend and understand all of it
+# i will work on this more
+
 def NumberChecker(Num):
     if Num < 2:
         print("Sorry You have to choose a number that is above 2 or is 2 itself")
