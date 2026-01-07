c60969d130f80ada136adc4482acd2d2342fc1a3
BoopYurNose <zacharybaca07@gmail.com>
2026-01-07 23:33:51 +0000

    :wq

---
+++ HireWheelStuff/OctoberChallenges/CodingPractice3.py
@@ -0,0 +1,11 @@
+def UniqueWordChecker(WordText):
+    if len(WordText) == len(set(WordText)):
+        print(f"Every character in this word {WordText} is a unique character")
+    else:
+        print(f"Every character in this word {WordText} is not a unique character")
+    
+print("Hello, welcome to the Unique Characters checker, \n This will check if all of your characters in the string that you input are all unique from eachother ")
+
+UserInput = input("Type in your characters:")
+
+UniqueWordChecker(UserInput)
