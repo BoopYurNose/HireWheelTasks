86c00cf001087ac6bd7709deed34d07bda5f7b10
BoopYurNose <zacharybaca07@gmail.com>
2026-01-12 22:17:55 +0000

    :wq

--- HireWheelStuff/OctoberChallenges/CodingPractie2.py
+++ HireWheelStuff/OctoberChallenges/CodingPractie2.py
@@ -7,18 +7,19 @@ AvailableShapes = {
     "Rectangle",
     "Triangle",
 }
+def MainMenu():
+    print("I can calculate the area of a shape for you. Which shape would you like me to calculate the area of? Here are your options below \n")
 
-print("I can calculate the area of a shape for you. Which shape would you like me to calculate the area of? Here are your options below \n")
+    for Shape in AvailableShapes:
+        print(Shape)
 
-for Shape in AvailableShapes:
-    print(Shape)
+    UserInput = input("Type in one of the options above!:")
 
-UserInput = input("Type in one of the options above!:")
+    for Shapes in AvailableShapes:
+        if UserInput.lower() in Shapes.lower():
+            print("This works")
 
-for Shapes in AvailableShapes:
-    if UserInput.lower() != Shapes.lower():
-        print("Sorry, this is not an option on the available shapes")
-        break
-    else:
-        print("This works great!")
-        break
+MainMenu()
+        
+
+    
