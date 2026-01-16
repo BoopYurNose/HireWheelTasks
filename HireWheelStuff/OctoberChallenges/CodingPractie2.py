3921b822dfdff70c1363e797020651d650a1aa24
BoopYurNose <zacharybaca07@gmail.com>
2026-01-16 17:55:03 +0000

    :wq

--- HireWheelStuff/OctoberChallenges/CodingPractie2.py
+++ HireWheelStuff/OctoberChallenges/CodingPractie2.py
@@ -1,5 +1,15 @@
 def AreaCalculator(Shape):
-    pass
+    if Shape == "sqaure":
+        print(f"You have picked {Shape} as your shape")
+        UserBase = input("What is your base: ")
+        UserHeight = input("What is your height: ")
+        if UserBase.isdigit() and UserHeight.isdigit():
+            print("this works both are digits")
+            UserBase = int(UserBase)
+            UserHeight = int(UserHeight)
+
+        SquareArea = UserBase * UserHeight / 2
+        print(f"Your area of your {Shape} is {SquareArea}")
 
 AvailableShapes = {
     "Sqaure",
@@ -17,9 +27,6 @@ def MainMenu():
 
     for Shapes in AvailableShapes:
         if UserInput.lower() in Shapes.lower():
-            print("This works")
+            AreaCalculator(UserInput.lower())
 
 MainMenu()
-        
-
-print("test")
