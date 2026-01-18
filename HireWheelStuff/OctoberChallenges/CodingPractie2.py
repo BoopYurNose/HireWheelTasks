ea56431c818ac432802bb535f516a98d440e3b5d
BoopYurNose <zacharybaca07@gmail.com>
2026-01-18 20:12:46 +0000

    :wq

--- HireWheelStuff/OctoberChallenges/CodingPractie2.py
+++ HireWheelStuff/OctoberChallenges/CodingPractie2.py
@@ -1,18 +1,47 @@
 def AreaCalculator(Shape):
-    if Shape == "sqaure":
-        print(f"You have picked {Shape} as your shape")
+    print(f"You have picked {Shape} as your shape")
+    if Shape == "rectangle":
         UserBase = input("What is your base: ")
         UserHeight = input("What is your height: ")
         if UserBase.isdigit() and UserHeight.isdigit():
-            print("this works both are digits")
             UserBase = int(UserBase)
             UserHeight = int(UserHeight)
+            RectangleArea = UserBase * UserHeight
+            print(f"Your area of your {Shape} is {RectangleArea}")
+            return
+        
 
-        SquareArea = UserBase * UserHeight / 2
-        print(f"Your area of your {Shape} is {SquareArea}")
+    elif Shape == "square":
+        UserLength = input("What is your Length: ")
+        if UserLength.isdigit():
+            UserLength = int(UserLength)
+            SquareArea = UserLength * UserLength
+            print(f"Your area of your {Shape} is {SquareArea}")
+            return
+        
+    elif Shape == "triangle":
+        UserBase = input("What is your base: ")
+        UserHeight = input("What is your height: ")
+        if UserBase.isdigit() and UserHeight.isdigit():
+            UserBase = int(UserBase)
+            UserHeight = int(UserHeight)
+            TriangleArea = UserBase * UserHeight / 2
+            print(f"Your area of your {Shape} is {TriangleArea}")
+            return
+        
+    elif Shape == "circle":
+        UserRadius = input("What is your radius: ")
+        if UserRadius.isdigit():
+            UserRadius = int(UserRadius)
+            UserRadius = UserRadius * UserRadius
+            CircleArea = UserRadius * 3.14
+            print(f"Your area of your {Shape} is {CircleArea}")
+            return
+
+
 
 AvailableShapes = {
-    "Sqaure",
+    "Square",
     "Circle",
     "Rectangle",
     "Triangle",
