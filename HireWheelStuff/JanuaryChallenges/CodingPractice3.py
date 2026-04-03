d0e0dbac84688dd406a1e95965e58abecf35b6a5
BoopYurNose <zacharybaca07@gmail.com>
2026-04-03 22:34:05 +0000

    :wq

--- HireWheelStuff/JanuaryChallenges/CodingPractice3.py
+++ HireWheelStuff/JanuaryChallenges/CodingPractice3.py
@@ -10,35 +10,48 @@ Output total words and unique word count.'''
 import string
 
 def UniqueWordChecker(SentenceInput):
+    #print(SentenceInput)
 
     TotalWords = len(SentenceInput.split())
-    Words = []
-    print(TotalWords)
+    print(f"You have: {TotalWords} words in your sentence")
+
+    for p in string.punctuation:
+        SetenceInput = SentenceInput.replace(p, "")
+
+    Words = (SentenceInput.split())
+    print(f"You have 2 unique words in your sentence {len(set(Words))}")
+
+    return
+
+    '''Words = [] Turns out I completely overcomplicated making this, I could've just used basic python string methods the entire time, but I did not know of their existence
+    #print(TotalWords)
 
 
     # I'm gonna take a break from this, make some sorta system where it iterates over each word depending on how many words there are which we checked by checking all the spaces
     # figure out how to all this in a single loop
     for i in range(0, TotalWords):
-        i += 1
-        Words.append({i: ""})
+        Words.append({i: ""}) # Creating a new table element of every different word in the sentence passed
         print(Words)
+        i += 1
+        
 
-    i = 1
-    for FirstLetters in SentenceInput:
-        if FirstLetters.isalnum():
-            print(FirstLetters)
-            Words[i].append(FirstLetters)
-            #Words[i] += str(FirstLetters)
-            # figure out how to append words for each specific dictionary word
+    Iterator = 0
+    for Letters in SentenceInput:
+        if Letters == " ":
+            print(f"FULL WORD HERE: {Words[Iterator]}")
+            Iterator += 1
+            print(" ")
+        elif Letters.isalnum():
+            #print(f"Display this one here {Letters}")
+            Words[Iterator] += Letters
+            print(f"Adding this to the dictionary {Iterator}: {Words[Iterator]}")
 
-        '''if Words == " " or not FirstLetters.isalnum():
-            Words.append(", ")
-            break'''
+    #print(Words[0])
     
 
     
     #Words = "".join(Words)
-    print(Words)
+    print(Words)'''
 
 
 
