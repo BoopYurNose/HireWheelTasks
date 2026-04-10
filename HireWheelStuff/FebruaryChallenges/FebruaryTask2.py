08ec6b1d7a37eb0df11ca82b962d551231e8852e
BoopYurNose <zacharybaca07@gmail.com>
2026-04-10 02:04:54 +0000

    :wq

---
+++ HireWheelStuff/FebruaryChallenges/FebruaryTask2.py
@@ -0,0 +1,7 @@
+#Generate and display the Fibonacci sequence up to N terms.
+
+'''What they mean by N terms is that they are reffering to the user input
+if N (userinput) is 5 the program will display the Fibonacci sequence up to 
+the number 5
+
+'''
