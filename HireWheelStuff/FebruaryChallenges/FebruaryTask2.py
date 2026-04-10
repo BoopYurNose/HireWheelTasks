17fe5c06059c66c6fc617afc2185e6d4dab6b81f
BoopYurNose <zacharybaca07@gmail.com>
2026-04-10 02:16:10 +0000

    :wq

--- HireWheelStuff/FebruaryChallenges/FebruaryTask2.py
+++ HireWheelStuff/FebruaryChallenges/FebruaryTask2.py
@@ -3,5 +3,27 @@
 '''What they mean by N terms is that they are reffering to the user input
 if N (userinput) is 5 the program will display the Fibonacci sequence up to 
 the number 5
-
 '''
+
+def FibonacciSequence(Amount):
+    #print(Amount)
+
+    
+    return
+
+def Main():
+    print("Welcome to the fibonacci sequence counter \n")
+
+    UserAmount = input("Please input the number value of fibonacci sequence you want the program to output to: ")
+
+    if UserAmount.isdigit():
+        pass
+    else:
+        print("Sorry try again you need to type in a valid whole number for this: \n")
+        Main()
+        return
+
+
+    FibonacciSequence(UserAmount)
+
+Main()
