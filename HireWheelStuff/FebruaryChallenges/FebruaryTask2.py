ce1da6cde42058c41c29b7a7497d966da87318c3
BoopYurNose <zacharybaca07@gmail.com>
2026-04-10 02:38:09 +0000

    :wq

--- HireWheelStuff/FebruaryChallenges/FebruaryTask2.py
+++ HireWheelStuff/FebruaryChallenges/FebruaryTask2.py
@@ -1,4 +1,5 @@
 #Generate and display the Fibonacci sequence up to N terms.
+# This was a lot easier than I thought it would be :3
 
 '''What they mean by N terms is that they are reffering to the user input
 if N (userinput) is 5 the program will display the Fibonacci sequence up to 
@@ -6,24 +7,30 @@ the number 5
 '''
 
 def FibonacciSequence(Amount):
-    #print(Amount)
-
+    StartingSequence = [0, 1]
+    while len(StartingSequence) < Amount:
+        StartingSequence.append(StartingSequence[-1] + StartingSequence[-2])
     
-    return
+    return StartingSequence
 
 def Main():
     print("Welcome to the fibonacci sequence counter \n")
 
-    UserAmount = input("Please input the number value of fibonacci sequence you want the program to output to: ")
+    UserAmount = input("Please input the number value of fibonacci sequence you want the program to output to (type a number bigger than 1 please): ")
 
     if UserAmount.isdigit():
         pass
+        UserAmount = int(UserAmount)
+        if UserAmount <= 1:
+            print("Please type a number bigger than 1 try again: \n")
+            Main()
+            return
     else:
         print("Sorry try again you need to type in a valid whole number for this: \n")
         Main()
         return
 
 
-    FibonacciSequence(UserAmount)
+    print(FibonacciSequence(UserAmount))
 
 Main()
